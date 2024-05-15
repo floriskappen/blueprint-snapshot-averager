afefd45b33247d3379b209e130085918201304de
Floris <kappenfloris@gmail.com>
2024-05-15 17:58:40 +0200

    feature: use memmap to speed up loading

--- src/load.rs
+++ src/load.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashMap, fs::File, io::{BufReader, Read}};
+use memmap::MmapOptions;
 
 use prost::Message;
 
@@ -6,14 +7,20 @@ use crate::proto::Blueprint as ProtoBlueprint;
 
 pub fn load_blueprint_file(filepath: &str) -> HashMap<Vec<u8>, Vec<u16>> {
     log::info!("Loading {}...", filepath);
-    let mut buf_reader = BufReader::new(File::open(filepath).expect("Error opening labels file"));
-    let mut buf = Vec::new();
-    buf_reader.read_to_end(&mut buf).expect("Error reading buffer from labels file");
-    let blueprint_proto = ProtoBlueprint::decode(&*buf).expect("Error decoding flop ClusteredDataLabels");
+
+    // Use memory-mapped file for potentially faster access
+    let file = File::open(filepath).expect("Error opening labels file");
+    let mmap = unsafe { MmapOptions::new().map(&file).expect("Error memory-mapping the file") };
+
+    // Decode directly from memory-mapped file
+    let blueprint_proto = ProtoBlueprint::decode(&*mmap).expect("Error decoding ProtoBlueprint");
+
     let mut blueprint = HashMap::new();
     for pair in blueprint_proto.pairs {
+        // Map directly into the target Vec
         let value = pair.value.into_iter().map(|x| x as u16).collect();
         blueprint.insert(pair.key, value);
     }
-    return blueprint
+
+    blueprint
 }
